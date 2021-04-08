aa01f76bec6a6e9d6560fab3f29756dee9e0ebff
Louis Orleans <louis@orleans.io>
2021-04-08 06:38:24 -0700

    🚧👷 switch to build matrix

--- dev/ci/build.sh
+++ dev/ci/build.sh
@@ -10,36 +10,48 @@ pkger
 
 mkdir -p "$mac_app_contents_dir/"{MacOS,Resources}
 
-# build ICNS
-mkdir "./assets/images/icon.iconset"
-for size in 128 256; do
-  convert "./assets/images/icon.png" -resize "${size}x${size}" "./assets/images/icon.iconset/icon_${size}x${size}.png"
-done
-png2icns "./assets/images/icon.icns" "./assets/images/icon.iconset/"*.png
-cp "./assets/images/icon.icns" "$mac_dir/.VolumeIcon.icns"
-cp "./assets/images/icon.icns" "$mac_app_contents_dir/Resources/icon.icns"
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  # build ICNS
+  mkdir "./assets/images/icon.iconset"
+  for size in 128 256; do
+    convert "./assets/images/icon.png" -resize "${size}x${size}" "./assets/images/icon.iconset/icon_${size}x${size}.png"
+  done
+  png2icns "./assets/images/icon.icns" "./assets/images/icon.iconset/"*.png
+  cp "./assets/images/icon.icns" "$mac_dir/.VolumeIcon.icns"
+  cp "./assets/images/icon.icns" "$mac_app_contents_dir/Resources/icon.icns"
 
-# package images
-cp "./assets/images/icon.png" "$mac_app_contents_dir/Resources/"
+  # package images
+  cp "./assets/images/icon.png" "$mac_app_contents_dir/Resources/"
 
-# template Info.plist
-eval "cat <<EOF
-$(<"./assets/Info.plist")
-EOF
-" 2> /dev/null > "$mac_app_contents_dir/Info.plist"
+  # template Info.plist
+  eval "cat <<EOF
+  $(<"./assets/Info.plist")
+  EOF
+  " 2> /dev/null > "$mac_app_contents_dir/Info.plist"
 
-# Prepare DMG assets
-cp "./assets/dmg-contents/USS.DS_Store" "$mac_dir/.DS_Store"
-ln -s "/Applications" "$mac_dir/Applications"
+  # # Prepare DMG assets
+  # cp "./assets/dmg-contents/USS.DS_Store" "$mac_dir/.DS_Store"
+  # ln -s "/Applications" "$mac_dir/Applications"
+fi
 
 # build
-GOOS=darwin  GOARCH=amd64 go build -o "$mac_app_contents_dir/MacOS/unrailed-save-scummer-amd64-darwin" .
-GOOS=linux   GOARCH=amd64 go build -o "./bin/unrailed-save-scummer-amd64-linux" .
-GOOS=windows GOARCH=amd64 go build -o "./bin/unrailed-save-scummer-amd64-windows.exe" .
+out="./bin/unrailed-save-scummer"
+case "$OSTYPE" in
+linux)
+  out="./bin/unrailed-save-scummer-amd64-linux" ;;
+darwin*)
+  out="$mac_app_contents_dir/MacOS/unrailed-save-scummer-amd64-darwin" ;;
+*)
+  echo "OS is $OSTYPE"
+  out="./bin/unrailed-save-scummer-amd64-windows.exe" ;;
+esac
 
-# Package macOS app into DMG
-case "$(uname)" in
-"Darwin")
+echo "Building binary"
+go build -o "$out" .
+
+case "$OSTYPE" in
+darwin*)
+  # Package macOS app into DMG
   width=600
   height=400
   create-dmg \
@@ -47,20 +59,15 @@ case "$(uname)" in
     --volicon "./assets/images/icon.icns" \
     --window-size $width $height \
     --icon-size $(expr $height / 4) \
-    --icon "Unrailed Save Scummer.app" $(expr $width / 4 - $height / 8) $(expr $height / 2 - $height / 8) \
+    --icon "Unrailed Save Scummer.app" $(expr $width / 4 - $height / 8) $(expr $height / 2 - $height / 6) \
     --hide-extension "Unrailed Save Scummer.app" \
-    --app-drop-link $(expr $width \* 3 / 4 - $height / 8) $(expr $height / 2 - $height / 8) \
+    --app-drop-link $(expr $width \* 3 / 4 - $height / 8) $(expr $height / 2 - $height / 6) \
     --hdiutil-quiet \
     --no-internet-enable \
-    "./bin/unrailed-save-scummer-amd64-darwin.dmg" "$mac_app_dir"
+    "./bin/unrailed-save-scummer-amd64-mac.dmg" "$mac_app_dir"
   ;;
-"Linux")
-  # dd if=/dev/zero of=/tmp/foo.dmg bs=1M count=64
-  # mkfs.hfsplus -v ThisIsFoo /tmp/foo.dmg
-  genisoimage \
-    -V "Unrailed Save Scummer" -D -R -apple -no-pad \
-    -o "./bin/unrailed-save-scummer-amd64-darwin.dmg" "$mac_dir"
+linux)
   ;;
 *)
-  >&2 echo "Unknown build platform!"
+  ;;
 esac
